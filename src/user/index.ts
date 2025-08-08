ade67c75a45a233f4aaa87a24419b857f8ec4f3d
Khing78 <khing78@odds.team>
2025-08-08 12:41:31 +0700

    remove: unused

--- src/user/index.ts
+++ src/user/index.ts
@@ -1,7 +1,10 @@
-import { fetchUserProfile } from './user';
 import { axiosHttpClient } from './httpClient';
+import { HttpClient } from './types';
 
 (async () => {
   const profile = await fetchUserProfile(1, axiosHttpClient);
   console.log(profile);
 })();
+function fetchUserProfile(arg0: number, axiosHttpClient: HttpClient) {
+  throw new Error('Function not implemented.');
+}
